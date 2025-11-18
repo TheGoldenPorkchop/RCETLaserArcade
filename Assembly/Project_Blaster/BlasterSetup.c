233b43f04a4d400fb964d85ce60a284bdc832120
andrew1593571 <andrewkeller2002@gmail.com>
2025-11-18 13:25:17 -0700

    Move existing code to Rev A. Init Rev. B

---
+++ Assembly/Project_Blaster/BlasterSetup.c
@@ -0,0 +1,129 @@
+/*
+   Filename:        BlasterSetup.c                                                          
+   Date:            November 18, 2025                                                    
+   File Version:    1                                                               
+   Author:          Andrew Keller                                                       
+   Company:         Idaho State University					    
+   Description:     Laser Arcade Blaster Setup Source Code File
+*/
+
+#include <xc.h>
+#include "BlasterSetup.h"
+
+void Setup_ConfigureOscillator(void){
+    OSCCON = 0b11101000; //Enable Internal osc, set to 4MHz
+    CLKRCON = 0x00; //Disable Clock Reference
+}
+
+/*
+volatile unsigned char SENDLEDRED __at(0x20);
+volatile unsigned char SENDLEDBLUE __at(0x21);
+volatile unsigned char SENDLEDGREEN __at(0x22);
+volatile unsigned char LEDNUMBER __at(0x23);
+volatile unsigned char LEDCOUNTER __at(0x24);
+
+extern void SENDLED(void);
+
+void ConfigureOptions(void){
+    OPTION_REG = 0b11001000;
+}
+
+void ConfigurePinFunctions(void){
+    APFCON1 = 0x00;
+    APFCON2 = 0x00;
+}
+
+void ConfigurePortA(void){
+    PORTA = 0x00; //clear port A
+    LATA = 0X00; 
+    TRISA = 0b10001001; //porta as outputs
+    ANSELA = 0b00001001; //porta as digital I/O, RA0 as analog, RA3 as VREF
+    WPUA = 0x00; //disable all pull-ups
+    ODCONA = 0x00; //disable open drain
+    SLRCONA = 0x00; //set maximum slew rate
+    INLVLA = 0x00; //set the port to TTL levels
+    IOCAP = 0x00; //disable rising edge interrupts
+    IOCAN = 0x00; //disable falling edge interrupts
+}
+
+void ConfigurePortB(void){
+    PORTB = 0x00; //clear port B
+    LATB = 0X00; 
+    TRISB = 0X01; //portb as outputs, RB0 as input
+    ANSELB = 0x00; //portb as digital I/O
+    WPUB = 0x00; //disable all pull-ups
+    ODCONB = 0x00; //disable open drain
+    SLRCONB = 0x00; //set maximum slew rate
+    INLVLB = 0x00; //set the port to TTL levels
+    IOCBP = 0x00; //disable rising edge interrupts
+    IOCBN = 0x00; //disable falling edge interrupts
+}
+
+void ConfigurePortC(void){
+    PORTC = 0x00; //clear port C
+    LATC = 0X00; 
+    TRISC = 0b11000000;
+    ANSELC = 0x00; //portC as digital I/O
+    WPUC = 0x00; //disable all pull-ups
+    ODCONC = 0x00; //disable open drain
+    SLRCONC = 0x00; //set maximum slew rate
+    INLVLC = 0x00; //set the port to TTL levels
+    IOCCP = 0x00; //disable rising edge interrupts
+    IOCCN = 0x00; //disable falling edge interrupts
+}
+
+void ConfigureCCP1(void){
+    TRISCbits.TRISC2 = 1; //disable PWM Output for CCP1 setup
+    CCP1CON = 0x0C; //set CCP1 to PWM Mode
+    CCPR1L = 0x00; //PW for 50%DC at 38kHz
+    T2CON = 0b00000011; //64x pre-scaler
+    PR2 = 0xFF; //Set PR2 to 25 for 38kHz period
+    TRISCbits.TRISC2 = 0; //enable PWM Output for CCP1
+    T2CONbits.TMR2ON = 1;
+}
+
+void ConfigurePSMC1(void){
+    PSMC1PRH = 0x00; //Set the period to 20ms
+    PSMC1PRL = 0x68;
+    PSMC1DCH = 0x00; //set the falling edge to centered servo
+    PSMC1DCL = 0x34;
+    PSMC1PHH = 0x00; //set the rising edge to always 0
+    PSMC1PHL = 0x00;
+    PSMC1CLK = 0b00110000; //Set PSMC1 clock to FOSC/8 (4MHz)
+    PSMC1STR0 = 0x01; //enable PSMC output 1
+    PSMC1POL = 0x00; //polarity is active high
+    PSMC1OEN = 0x01; //output enabled A
+    PSMC1PRS = 0x01; //only time base causes a period event
+    PSMC1PHS = 0x01; //only time base causes a rising edge event
+    PSMC1DCS = 0x01; //only time base causes a falling edge event
+    PSMC1MDL = 0b11101000; //PSMC1 is modulated by active low signal at PSMC1IN
+    PSMC1CON = 0b11000000; //enable PSMC and load changes in
+}
+
+void ConfigureUART(void){
+    SPBRGH = 0x03; //UART BAUD RATE OF 2400 W/ 32MHz OSCILLATOR
+    SPBRGL = 0x40;
+    BAUDCON = 0b00001000; //16-BIT BAUD RATE GENERATOR
+    RCSTA = 0b10010000; //ENABLE UART PORT, ENABLE RECEIVER
+    TXSTA = 0b00100000; //ENABLE TRANSMITTER, ASYNCHRONOUS
+}
+
+void ConfigureADC(void){
+    ADCON0 = 0b11000001; //Enable ADC for AN0, 10-bit result
+    ADCON1 = 0b00010001; //FOSC/8 ADC clock, VREF+ on RA3, VREF- on VSS
+    ADCON2 = 0x00; //clear everything
+}
+
+void ConfigureInterrupts(void){
+    INTCON = 0b01000000;
+    PIE1 = 0b01000000;
+    PIE2 = 0x00;
+    PIE3 = 0x00;
+    PIE4 = 0x00;
+    PIR1 = 0x00;
+    PIR2 = 0x00;
+    PIR3 = 0x00;
+    PIR4 = 0x00;
+    INTCONbits.GIE = 1;
+}
+ */
---
+++ Assembly/Project_Blaster/BlasterSetup.h
@@ -0,0 +1,29 @@
+/*
+   Filename:        BlasterSetup.h                                                          
+   Date:            November 18, 2025                                                    
+   File Version:    1                                                               
+   Author:          Andrew Keller                                                       
+   Company:         Idaho State University					    
+   Description:     Laser Arcade Blaster Setup Header File
+*/
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef BLASTER_SETUP
+#define	BLASTER_SETUP
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+//Function Prototypes
+void Setup_ConfigureOscillator(void);
+void Setup_ConfigureOptions(void);
+void Setup_ConfigurePinFunctions(void);
+void Setup_ConfigurePortA(void);
+void Setup_ConfigurePortB(void);
+void Setup_ConfigurePortC(void);
+void Setup_ConfigureLEDPWM(void);
+void Setup_ConfigureLasers(void);
+void Setup_ConfigureAudio(void);
+
+#endif	/* Blaster_SETUP */
+
